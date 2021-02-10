78bbdc42592bafccc8ee6ce13365a4819c8a9b74
Priyanka <priyanka.manjush@gmail.com>
2021-02-10 14:08:42 +0400

    header,footer,login page done

---
+++ resources/views/master.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Ecommerce Project</title>
+</head>
+<body>
+<!-- {{View::make('header')}} -->
+<x-layouts.header></x-layouts.header>
+    @yield('content')
+    {{View::make('footer')}}
+</body><style>
+.custom-login{
+    height:500px;
+    padding-top:50px;
+}
+</style>
+
+</html>
+
